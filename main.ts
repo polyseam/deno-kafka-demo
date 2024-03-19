2f1366d03fd21bc4e9cdad17052fa1241bad795a
Matt Johnston <m@ttjohnston.com>
2024-03-19 20:10:31 -0300

    hello again

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
 serve((_req: Request) => {
-  return new Response("Hello World", {});
+
+  return new Response("Hello Tamika", {});
 });

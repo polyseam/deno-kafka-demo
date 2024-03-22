9d788883524333f66413b4bac56e838c4be85a40
Matt Johnston <m@ttjohnston.com>
2024-03-22 17:38:32 -0300

    add kafka

--- main.ts
+++ main.ts
@@ -1,4 +1,38 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import { CompressionTypes, Kafka } from "npm:kafkajs";
+import os from "node:os";
+
+const password = Deno.env.get("KAFKA_PASSWORD");
+
+const redpanda = new Kafka({
+  brokers: [
+    "cnuujbjiuvqvkcfi58k0.any.us-east-1.mpx.prd.cloud.redpanda.com:9092",
+  ],
+  ssl: {},
+  sasl: {
+    mechanism: "SCRAM-SHA-256",
+    username: "deno-fns",
+    password,
+  },
+});
+
+const producer = redpanda.producer();
+
+const sendMessage = (msg: string) => {
+  return producer.send({
+    topic: "test",
+    compression: CompressionTypes.GZIP,
+    messages: [{
+      // Messages with the same key are sent to the same topic partition for
+      // guaranteed ordering
+      key: os.hostname(),
+      value: JSON.stringify(msg),
+    }],
+  })
+    .catch((e: { message: any }) => {
+      console.error(`Unable to send message: ${e.message}`, e);
+    });
+};
 
 // roll dice
 const rollDice = () => {
@@ -6,6 +40,7 @@ const rollDice = () => {
 };
 
 serve((_req: Request) => {
-  
-  return new Response(`you rolled ${rollDice()}`, {});
+  const diceRollResult = rollDice();
+  sendMessage("roll: " + diceRollResult);
+  return new Response(`you rolled ${diceRollResult}`, {});
 });

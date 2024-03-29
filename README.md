a5123ca36577370e21d89aadf139b04e769edbe4
Matt Johnston <m@ttjohnston.com>
2024-03-29 00:04:29 -0300

    everything in serve

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ This repository contains a demo of how to connect to Redpanda Cloud using the
 ## usage
 
 Visit
-[redpanda-demo.deno.dev/yourname/roll](https://redpanda-demo.deno.dev/anonymous/roll) to
-produce an event in the `"roll"` topic, and receive a response.
+[redpanda-demo.deno.dev/yourname/roll](https://redpanda-demo.deno.dev/anonymous/roll)
+to produce an event in the `"roll"` topic, and receive a response.
--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 import { CompressionTypes, Kafka, Message } from "npm:kafkajs@2.2.4";
 
-const username = Deno.env.get("KAFKA_USERNAME");
-const password = Deno.env.get("KAFKA_PASSWORD");
-const broker = Deno.env.get("KAFKA_BROKER");
 const SASL_MECHANISM = "scram-sha-256";
 
 type DiceRollEvent = {
@@ -11,54 +8,9 @@ type DiceRollEvent = {
   username: string;
 };
 
-if (!username) {
-  throw new Error("Please set the 'KAFKA_USERNAME' environment variable");
-}
-
-if (!password) {
-  throw new Error("Please set the 'KAFKA_PASSWORD' environment variable");
-}
-
-if (!broker) {
-  throw new Error("Please set the 'KAFKA_BROKER' environment variable");
-}
-
-const redpanda = new Kafka({
-  brokers: [broker],
-  ssl: {},
-  sasl: {
-    mechanism: SASL_MECHANISM,
-    username,
-    password,
-  },
-});
-
-const producer = redpanda.producer();
-
 type EasyMsg = {
   key: string;
   value: Record<string, unknown>;
-};
-
-const sendMessage = (topic: string, msg: EasyMsg) => {
-  // Messages with the same key are sent to the same topic partition for
-  // guaranteed ordering
-  const messages: Array<Message> = [
-    {
-      key: msg.key,
-      value: JSON.stringify(msg.value),
-    },
-  ];
-
-  return producer
-    .send({
-      topic,
-      compression: CompressionTypes.GZIP,
-      messages,
-    })
-    .catch((e: Error) => {
-      console.error(`Unable to send message: ${e.message}`, e);
-    });
 };
 
 // roll dice
@@ -67,6 +19,59 @@ const rollDice = () => {
 };
 
 serve((req: Request) => {
+  // get kafka credentials
+  const kafkaUsername = Deno.env.get("KAFKA_USERNAME");
+  const password = Deno.env.get("KAFKA_PASSWORD");
+  const broker = Deno.env.get("KAFKA_BROKER");
+
+  // check if credentials are set
+  if (!kafkaUsername) {
+    throw new Error("Please set the 'KAFKA_USERNAME' environment variable");
+  }
+
+  if (!password) {
+    throw new Error("Please set the 'KAFKA_PASSWORD' environment variable");
+  }
+
+  if (!broker) {
+    throw new Error("Please set the 'KAFKA_BROKER' environment variable");
+  }
+
+  // setup client
+  const redpanda = new Kafka({
+    brokers: [broker],
+    ssl: {},
+    sasl: {
+      mechanism: SASL_MECHANISM,
+      username: kafkaUsername,
+      password,
+    },
+  });
+
+  // create producer
+  const producer = redpanda.producer();
+
+  const sendMessage = (topic: string, msg: EasyMsg) => {
+    // Messages with the same key are sent to the same topic partition for
+    // guaranteed ordering
+    const messages: Array<Message> = [
+      {
+        key: msg.key,
+        value: JSON.stringify(msg.value),
+      },
+    ];
+
+    return producer
+      .send({
+        topic,
+        compression: CompressionTypes.GZIP,
+        messages,
+      })
+      .catch((e: Error) => {
+        console.error(`Unable to send message: ${e.message}`, e);
+      });
+  };
+
   const region = Deno.env.get("DENO_REGION") || "unknown";
   const roll = rollDice();
   const segments = new URL(req.url).pathname.split("/");

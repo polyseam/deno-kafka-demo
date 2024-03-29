98f2d12a17972b7c7d9efb8657879f323301e3d2
Matt Johnston <m@ttjohnston.com>
2024-03-28 23:27:30 -0300

    roll topic producer

--- main.ts
+++ main.ts
@@ -1,35 +1,63 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
-import { CompressionTypes, Kafka } from "npm:kafkajs";
-import os from "node:os";
+import { CompressionTypes, Kafka, Message } from "npm:kafkajs@2.2.4";
 
+const username = Deno.env.get("KAFKA_USERNAME");
 const password = Deno.env.get("KAFKA_PASSWORD");
+const broker = Deno.env.get("KAFKA_BROKERS");
+
+type DiceRollEvent = {
+  roll: number;
+};
+
+if (!username) {
+  console.error("Please set the 'KAFKA_USERNAME' environment variable");
+  Deno.exit(1);
+}
+
+if (!password) {
+  console.error("Please set the 'KAFKA_PASSWORD' environment variable");
+  Deno.exit(2);
+}
+
+if (!broker) {
+  console.error("Please set the 'KAFKA_BROKER' environment variable");
+  Deno.exit(3);
+}
 
 const redpanda = new Kafka({
-  brokers: [
-    "cnuujbjiuvqvkcfi58k0.any.us-east-1.mpx.prd.cloud.redpanda.com:9092",
-  ],
+  brokers: [broker],
   ssl: {},
   sasl: {
-    mechanism: "SCRAM-SHA-256",
-    username: "deno-fns",
+    mechanism: "scram-sha-256",
+    username,
     password,
   },
 });
 
 const producer = redpanda.producer();
 
-const sendMessage = (msg: string) => {
-  return producer.send({
-    topic: "test",
-    compression: CompressionTypes.GZIP,
-    messages: [{
-      // Messages with the same key are sent to the same topic partition for
-      // guaranteed ordering
-      key: os.hostname(),
-      value: JSON.stringify(msg),
-    }],
-  })
-    .catch((e: { message: any }) => {
+type EasyMsg = {
+  key: string;
+  value: Record<string, unknown>;
+};
+
+const sendMessage = (topic: string, msg: EasyMsg) => {
+  // Messages with the same key are sent to the same topic partition for
+  // guaranteed ordering
+  const messages: Array<Message> = [
+    {
+      key: msg.key,
+      value: JSON.stringify(msg.value),
+    },
+  ];
+
+  return producer
+    .send({
+      topic,
+      compression: CompressionTypes.GZIP,
+      messages,
+    })
+    .catch((e: Error) => {
       console.error(`Unable to send message: ${e.message}`, e);
     });
 };
@@ -39,8 +67,31 @@ const rollDice = () => {
   return Math.floor(Math.random() * 6) + 1;
 };
 
-serve((_req: Request) => {
-  const diceRollResult = rollDice();
-  sendMessage("roll: " + diceRollResult);
-  return new Response(`you rolled ${diceRollResult}`, {});
+serve((req: Request) => {
+  const roll = rollDice();
+  const segments = new URL(req.url).pathname.split("/");
+
+  if (segments.length !== 3) {
+    return new Response("Bad Request: please use /roll/:username", {
+      status: 400,
+    });
+  }
+
+  const action = segments[1];
+  const username = segments[2];
+
+  const value: DiceRollEvent = { roll };
+  const date = new Date().toISOString();
+
+  if (action === "roll") {
+    sendMessage("roll", {
+      key: username,
+      value,
+    });
+    return new Response(`${username} rolled ${roll} at ${date}`, {});
+  }
+
+  return new Response("Bad Request: please use /roll/:username", {
+    status: 400,
+  });
 });
